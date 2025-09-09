fdde51a73e98fc8678b59160c58cbeb2c29c2b9a
Pie-De-Limon69 <choque2015li@gmail.com>
2025-09-09 19:34:16 -0400

    Clase 1 funcional

--- tests/title-multi.spec.js
+++ tests/title-multi.spec.js
@@ -1,16 +1,16 @@
 const { test, expect } = require("@playwright/test");
 
 const casos = [
-  { url: "https://example.com/", titulo: "Example Domain" },
+  { url: "https://example.com/", esperado: "Example Domain" },
   //{ url: "https://example.com/", titulo: "/Example/" },
-  { url: "https://www.google.com/", titulo: "Google" },
+  { url: "https://www.google.com/", esperado: "Google" },
 ];
 
 for (const { url, esperado } of casos) {
   test(`valida el texto del titulo correcto en ${url}`, async ({ page }) => {
     //abre la pagina example.com
     await page.goto(url);
-    await sleep(5000);
+    await sleep(3000);
 
     //verificacion del titulo exacto
     await expect(page).toHaveTitle(esperado);
